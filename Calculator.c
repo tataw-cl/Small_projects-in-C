56482b7c958dad302b0c123f5e63e1135891d8a9
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2023-12-28 23:40:43 +0100

    added the raised to functionality

--- Calculator.c
+++ Calculator.c
@@ -1,10 +1,12 @@
+//Simple C program for a simple calculator
 #include <stdio.h>
+#include<math.h>
 
 int main() {
     char operator;
     double num1, num2;
 
-    printf("Enter an operator (+, -, *, /): ");
+    printf("Enter an operator (+, -, *, /, ^): ");
     scanf("%c", &operator);
 
     printf("Enter two operands: ");
@@ -26,8 +28,10 @@ int main() {
             else
                 printf("Error! Division by zero is not allowed.");
             break;
+            case('^'):
+            printf("%.2lf ^ %.2lf = %.2lf", num1,num2, pow(num1,num2));
         default:
-            printf("Error! Invalid operator");
+            printf("Error! Invalid operator for this calculator");
     }
 
     return 0;
